608f8257de628b78a23b80420b3e358bf1ceb97d
jeenklynn <116089650+tgulboy@users.noreply.github.com>
2022-11-29 09:13:38 +0300

    29.11.2022

--- restaurant/home/models.py
+++ restaurant/home/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+  name = models.CharField(max_length=255)
+  desc = models.CharField(max_length=255)
+  Kinds = models.TextChoices('Kinds','Breakfast Meals Snacks Desserts Drinks')
+  kind = models.CharField(blank=True, choices=Kinds.choices, max_length=10)
+  #img = models.ImageField(upload_to='uploads/')
+  price = models.DecimalField(max_digits=8, decimal_places=2)
+
+  def __str__(self):
+          return self.name
+
+class Reservation(models.Model): 
+  fullname = models.CharField(max_length=255)
+  email = models.EmailField(max_length=255)
+  phone_number = models.CharField(max_length=12)
+  people = models.IntegerField()
+  date = models.DateField()
+  time = models.TimeField()
+  message = models.TextField()
+
+  def __str__(self):
+          return self.fullname
+
+class Gallery(models.Model): 
+  image = models.CharField(max_length=255)
+  Types = models.TextChoices('Types','Food Drink Restaurant Dinner Dessert')
+  type = models.CharField(blank=True, choices=Types.choices, max_length=20)
+
+  def __str__(self):
+          return str(self.image)
+
+class Subscription(models.Model): 
+  email = models.EmailField(max_length=255)
+
+  def __str__(self):
+          return str(self.email)
+
+class Chef(models.Model): 
+  fullname = models.CharField(max_length=255)
+  Types = models.TextChoices('Types','Head Chef, Pizza Chef, Grill Chef, Burger Chef')
+  type = models.CharField(blank=True, choices=Types.choices, max_length=20)  
+  photo = models.ImageField(upload_to='uploads/')
+  facebook = models.CharField(max_length=255)
+  twitter = models.CharField(max_length=255)
+  linkedin = models.CharField(max_length=255)
+
+  def __str__(self):
+          return str(self.fullname)
+
+class Blog(models.Model): 
+  title = models.CharField(max_length=255)
+  author = models.CharField(max_length=255)
+  banner = models.ImageField(upload_to='uploads/')
+  release_date = models.DateField()
+  content = models.TextField()
+
+  def __str__(self):
+          return str(self.title)
+
+class Contact(models.Model): 
+  name = models.CharField(max_length=255)
+  email = models.EmailField(max_length=255)
+  subject = models.CharField(max_length=255)
+  message = models.TextField()
+
+  def __str__(self):
+          return str(self.name)
--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from home.views import index
+
 urlpatterns = [
     path('', index, name="index"),
     path('admin/', admin.site.urls),
